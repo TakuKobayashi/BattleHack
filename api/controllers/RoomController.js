884f8fc705c2bbdb64db3ff2652d4c68f3f205f7
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-06-14 04:00:35 +0900

    answer多分できた

--- api/controllers/RoomController.js
+++ api/controllers/RoomController.js
@@ -23,6 +23,7 @@ module.exports = {
     User.findOne({authToken: req.param('authToken')}).exec(function(err, user){
       Room.findOne({finishFlag: false}).exec(function(err, room){
         //まだ終わっていなくて、最後に答える人がいないRoomがあればそこに入れる
+        //TODO 前の人が書いた絵とか答えを返す
         if(room){
           RoomUser.findOne({roomId: room.id, answeredFlag: false, userId: user.id}).exec(function(err, ru){
             if(ru){
@@ -31,7 +32,7 @@ module.exports = {
               if(room.drawNumber > 0){
                 MstDrawingTheme.findOne({id: room.mstDrawingThemeId}).exec(function(err, theme){
                   room.drawNumber = room.drawNumber - 1;
-                  romm.save(function(err){ console.log(err); });
+                  room.save(function(err){ console.log(err); });
                   RoomUser.create({userId: user.id, roomId: room.id, drawingCount: theme.repeatNumber - room.drawNumber, answerFlag: room.drawNumber % 2 == 0}).exec(function(err, roomUser){
                     res.json(roomUser);
                   });
@@ -46,5 +47,29 @@ module.exports = {
         }
       });
     });
+  },
+  answer: function (req, res) {
+    RoomUser.findOne({id: req.param('roomUserId')}).exec(function(err, roomUser){
+      Room.findOne({id: roomUser.room}).exec(function(err, room){
+        roomUser.answeredFlag = true;
+        roomUser.answerWord = req.param('answer');
+        roomUser.save(function(err){ console.log(err); });
+        if(room.drawNumber <= 0){
+          room.finishFlag = true;
+          room.save(function(err){ console.log(err); });
+        }
+        res.send();
+        res.status(200);
+      });
+    });
+  },
+  sendDraw: function (req, res) {
+    var uploadFile = req.file('uploadFile');
+    console.log(uploadFile);
+    uploadFile.upload(function onUploadComplete (err, files) {				
+      //Files will be uploaded to .tmp/uploads
+      if (err) return res.serverError(err);
+        console.log(files);
+      });
   }
 }
